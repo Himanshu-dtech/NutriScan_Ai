be8b43290fa096a42314d248ae30019fad224a80
Himanshu <himanshu69894@gmail.com>
2026-01-07 11:54:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ llm = ChatGroq(
 )
 
 llm2 = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-1.5-flash",
     temperature=0.2,
     google_api_key=os.getenv("GOOGLE_API_KEY"),
 )
